05f6df763b167c2f52ba933bbecd1d8fa14fdc89
yanSilva <yansiba84@gmail.com>
2020-07-25 17:28:09 -0300

    Iniciando o desenvolvimento do Formulário

---
+++ scripts/models/user.js
@@ -0,0 +1,12 @@
+class User {
+    constructor(name, gender, dateBirth, country, email, pass, admin, photo) {
+       this.name = name;
+       this.gender = gender;
+       this.dateBirth = dateBirth;
+       this.country = country;
+       this.email = email;
+       this.pass = pass;
+       this.admin = admin;
+       this.photo = photo; 
+    }
+}
